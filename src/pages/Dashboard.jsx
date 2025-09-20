9791aa04cf2ed5e953885a171268408fa0f9dfaf
vmanidev <vmanidev@outlook.com>
2025-09-21 02:04:01 +0530

    feat(ui): add sections to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="app-title">BMI Calculator</div>
+      <div className="gender"></div>
+      <div className="height-slider"></div>
+      <div className="age-and-weight"></div>
+      <button>Calculate Your BMI</button>
+    </div>
+  );
 }
