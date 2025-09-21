a59cd380dd7a9ecae8f562b985d6c6b7e3fc5133
vmanidev <vmanidev@outlook.com>
2025-09-21 13:36:48 +0530

    feat(ui): add input fields

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,24 @@ export default function Dashboard() {
     <div className={styles.dashboardContainer}>
       <div className="app-title">BMI Calculator</div>
       <div className={styles.gender}>
-        <div className="card">Male</div>
-        <div className="card">Female</div>
+        <span class="card material-icons material-symbols-outlined">male</span>
+        <span class="card material-icons material-symbols-outlined">
+          female
+        </span>
       </div>
-      <div className="heightSlider card"></div>
+      <div className={`${styles.heightSlider} card`}>
+        <div className="card-title-container">
+          <span className="card-title">Height</span>
+          <span className="card-subtitle">165 CM</span>
+        </div>
+        <input
+          id="heightScale"
+          name="height"
+          type="range"
+          min="100"
+          max="250"
+        />
+      </div>
       <div className={styles.weightAndAge}>
         <div className="card">Age</div>
         <div className="card">Weight</div>
