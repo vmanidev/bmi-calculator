9e5a57052d637e5758911fcb15f389792cba8eb6
vmanidev <vmanidev@outlook.com>
2025-09-21 21:30:24 +0530

    feat(ui): add button to recalculate BMI and navigate back to dashboard

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,13 +1,17 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { calculateBmi } from "../util/bmi.js";
 
 import styles from "./Result.module.css";
 
 export default function Result() {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const { height, weight } = location.state;
   const { bmi, status } = calculateBmi({ height, weight });
-  
+
+  const reCalculateBmi = () => navigate("/");
+
   return (
     <div className={styles.resultContainer}>
       <div>Your Results</div>
@@ -15,6 +19,7 @@ export default function Result() {
         <div>{status}</div>
         <div>{bmi}</div>
       </div>
+      <button onClick={reCalculateBmi}>Re-Calculate Your BMI</button>
     </div>
   );
 }
