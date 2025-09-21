d7950451fd064245f921aba399ef6710be04e759
vmanidev <vmanidev@outlook.com>
2025-09-21 23:19:16 +0530

    fix(bmi): fix incorrect formula for BMI calculation

--- src/util/bmi.js
+++ src/util/bmi.js
@@ -5,11 +5,11 @@ export function calculateBmi({ height, weight }) {
 }
 
 export function getBmiStatus(bmi) {
-    if (bmi <= 18.5) return "Underweight";
-    else if (bmi > 18.5 && bmi < 24.9) return "Normal";
-    else if (bmi >= 25 && bmi < 29.9) return "Overweight";
-    else if (bmi >= 30 && bmi < 34.9) return "Obesity Class I (Moderate)";
-    else if (bmi >= 35 && bmi < 39.9) return "Obesity Class II (Severe)";
+    if (bmi < 18.5) return "Underweight";
+    else if (bmi >= 18.5 && bmi <= 24.9) return "Normal";
+    else if (bmi >= 25 && bmi <= 29.9) return "Overweight";
+    else if (bmi >= 30 && bmi <= 34.9) return "Obesity Class I (Moderate)";
+    else if (bmi >= 35 && bmi <= 39.9) return "Obesity Class II (Severe)";
     else if (bmi >= 40) return "Obesity Class III (Very severe / Morbid)";
     else return "Oops… Something went wrong. Please try again.";
 }
