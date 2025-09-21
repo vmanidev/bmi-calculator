09a74568fd528e322524714c2fb3973edb9c270f
vmanidev <vmanidev@outlook.com>
2025-09-21 12:33:26 +0530

    feat(layout): add dashboard layout with styles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,10 +3,16 @@ import styles from "./Dashboard.module.css";
 export default function Dashboard() {
   return (
     <div className={styles.dashboardContainer}>
-      <div className={`${styles.fullGridCol} app-title`}>BMI Calculator</div>
-      <div className="gender"></div>
-      <div className="heightSlider"></div>
-      <div className="ageAndWeight"></div>
+      <div className="app-title">BMI Calculator</div>
+      <div className={styles.gender}>
+        <div className="card">Male</div>
+        <div className="card">Female</div>
+      </div>
+      <div className="heightSlider card"></div>
+      <div className={styles.weightAndAge}>
+        <div className="card">Age</div>
+        <div className="card">Weight</div>
+      </div>
       <button className={styles.fullGridCol}>Calculate Your BMI</button>
     </div>
   );
