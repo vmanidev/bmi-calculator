ee3601425d5e182dfb9128f1388471bccee3e5a5
vmanidev <vmanidev@outlook.com>
2025-09-21 20:32:18 +0530

    feat: extract form data from location.state and display BMI and status

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,3 +1,20 @@
+import { useLocation } from "react-router-dom";
+import { calculateBmi } from "../util/bmi";
+
+import styles from "./result.module.css";
+
 export default function Result() {
-  return <div>Result</div>;
+  const location = useLocation();
+  const { height, weight } = location.state;
+  const { bmi, status } = calculateBmi({ height, weight });
+  
+  return (
+    <div className={styles.resultContainer}>
+      <div>Your Results</div>
+      <div className={`${styles.result} card`}>
+        <div>{status}</div>
+        <div>{bmi}</div>
+      </div>
+    </div>
+  );
 }
